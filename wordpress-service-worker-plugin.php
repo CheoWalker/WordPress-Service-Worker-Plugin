8c2cc13a5a245a14858bce0738058bd683119441
Cheo Walker <cwalker@dzwebcreations.com>
2015-07-06 10:12:29 -0400

    Refactor main PHP for boilerplate

--- wordpress-service-worker-plugin.php
+++ wordpress-service-worker-plugin.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           wordpress_service_worker_plugin
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin URI:        http://example.com/wordpress-service-worker-plugin-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress dashboard.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       wordpress-service-worker-plugin
  * Domain Path:       /languages
  */
 
@@ -32,30 +32,30 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-wordpress-service-worker-plugin-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_wordpress_service_worker_plugin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-service-worker-plugin-activator.php';
+	wordpress_service_worker_plugin_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-wordpress-service-worker-plugin-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_wordpress_service_worker_plugin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-service-worker-plugin-deactivator.php';
+	wordpress_service_worker_plugin_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_wordpress_service_worker_plugin' );
+register_deactivation_hook( __FILE__, 'deactivate_wordpress_service_worker_plugin' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * dashboard-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-wordpress-service-worker-plugin.php';
 
 /**
  * Begins execution of the plugin.
@@ -66,10 +66,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_wordpress_service_worker_plugin() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new wordpress_service_worker_plugin();
 	$plugin->run();
 
 }
-run_plugin_name();
+run_wordpress_service_worker_plugin();
